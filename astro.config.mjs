08f980372bcea8cb46199e04048661865402e1ec
NikosZar <65125459+NikosZar@users.noreply.github.com>
2025-11-01 02:48:05 -0700

    Astro Islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://delightful-sprite-709d70.netlify.app"
+  site: "https://delightful-sprite-709d70.netlify.app",
+  integrations: [preact()]
 });
